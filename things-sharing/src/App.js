7424df8a93414da7ed1a4bd58d4f289767ccb059
adampekala <adaspekala@wp.pl>
2023-05-16 22:43:07 +0200

    creating LogInComponent

--- things-sharing/src/App.js
+++ things-sharing/src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import HomePage from "./A_HomePage/home";
 import {HashRouter, Routes, Route} from "react-router-dom";
 import SignUpComponent from "./B_SignUpComponent/signUpComponent";
+import LogInComponent from "./B_LogInComponent/logInComponent";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage/>}/>
           <Route path='register' element={<SignUpComponent/>}/>
-          {/*<Route path='logIn' element={<LogInComponent/>}/>*/}
+          <Route path='logIn' element={<LogInComponent/>}/>
           {/*<Route path='logOut' element={<LogOutComponent/>}/>*/}
           {/*<Route path='form' element={<FormComponent/>}/>*/}
         </Routes>
---
+++ things-sharing/src/B_LogInComponent/logInComponent.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import decoration from '../Z_Images/Decoration.png'
+
+const LogInComponent = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [passwordRepeat, setPasswordRepeat] = useState('')
+
+    return (
+        <>
+            <div className='loginPanel'>
+                <button type='button' className='loginPanel_logInBtn'>Zaloguj</button>
+                <button type='button' className='loginPanel_signInBtn'>Załóż konto</button>
+            </div>
+            <ul className='navigation'>
+                <li>Start</li>
+                <li>O co chodzi?</li>
+                <li>O nas</li>
+                <li>Fundacja i organizacje</li>
+                <li>Kontakt</li>
+            </ul>
+            <form>
+                <h1>Zaloguj się</h1>
+                <img src={decoration} alt='dekoracja'/>
+                <div>
+                    <label htmlFor='email'>Email</label>
+                    <input id='email' type='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <label htmlFor='password'>Hasło</label>
+                    <input id='password' type='password' value={password}
+                           onChange={(e) => setPassword(e.target.value)}/>
+                    <label htmlFor='passwordRepeat'>Powtórz hasło</label>
+                    <input id='passwordRepeat' type='password' value={passwordRepeat}
+                           onChange={(e) => setPasswordRepeat(e.target.value)}/>
+
+                </div>
+                <div>
+                    <button type='submit'>Zaloguj się</button>
+                    <button>Załóż konto</button>
+                </div>
+            </form>
+        </>
+
+    )
+}
+
+export default LogInComponent;
