c3b1de74e01268ad5f4841e6095b0c5a90492772
adampekala <adaspekala@wp.pl>
2023-05-16 22:23:27 +0200

    developing routing in App.js

--- things-sharing/src/App.js
+++ things-sharing/src/App.js
@@ -1,9 +1,20 @@
 import './App.scss';
 import HomePage from "./A_HomePage/home";
+import {HashRouter, Routes, Route} from "react-router-dom";
+import SignUpComponent from "./B_SignUpComponent/signUpComponent";
 
 function App() {
   return (
-    <HomePage/>
+      <HashRouter>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='register' element={<SignUpComponent/>}/>
+          {/*<Route path='logIn' element={<LogInComponent/>}/>*/}
+          {/*<Route path='logOut' element={<LogOutComponent/>}/>*/}
+          {/*<Route path='form' element={<FormComponent/>}/>*/}
+        </Routes>
+      </HashRouter>
+
   );
 }
 
---
+++ things-sharing/src/B_SignUpComponent/signUpComponent.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import decoration from '../Z_Images/Decoration.png'
+
+const SignUpComponent = () => {
+
+    return (
+        <form>
+            <h1>Załóż konto</h1>
+            <img src={decoration} alt='dekoracja'/>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </form>
+    )
+}
+
+export default SignUpComponent;
