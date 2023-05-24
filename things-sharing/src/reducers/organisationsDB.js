50915443898333bd36bb0bcde734aecc78d991dc
adampekala <adaspekala@wp.pl>
2023-05-24 21:26:01 +0200

    developing and styling form component

--- things-sharing/src/reducers/organisationsDB.js
+++ things-sharing/src/reducers/organisationsDB.js
@@ -6,95 +6,95 @@ import {
 
 
 const initialState = [
-    {
-        name: 'Fundacja "Dbam o zdrowie"',
-        mission: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
-        needs: ['ubrania', 'jedzenie', 'sprzęt AGD', 'meble', 'zabawki'],
-        bags: 3,
-        collections: 6,
-        type: 'Fundacja'
-    },
-
-    {
-        name: 'Fundacja "Dla dzieci"',
-        mission: 'Pomoc dzieciom z ubogich rodzin.',
-        needs: ['ubrania', 'meble', 'zabawki'],
-        bags: 2,
-        collections: 1,
-        type: 'Fundacja'
-    },
-
-    {
-        name: 'Fundacja "Bez domu"',
-        mission: 'Pomoc dla osób nie posiadających miejsca zamieszkania.',
-        needs: ['ubrania', 'jedzenie', 'ciepłe koce'],
-        bags: 3,
-        collections: 2,
-        type: 'Fundacja'
-    },
-
-    {
-        name: 'Fundacja "Dbam2"',
-        mission: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
-        needs: ['ubrania', 'jedzenie', 'sprzęt AGD', 'meble', 'zabawki'],
-        bags: 3,
-        collections: 4,
-        type: 'Fundacja'
-    },
-
-    {
-        name: 'Fundacja "Dbam o zdrowie 2"',
-        mission: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
-        needs: ['ubrania', 'jedzenie', 'sprzęt AGD', 'meble', 'zabawki'],
-        bags: 3,
-        collections: 2,
-        type: 'Fundacja'
-    },
-
-    {
-        name: 'Fundacja "Zdrowie"',
-        mission: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
-        needs: ['ubrania', 'jedzenie', 'sprzęt AGD', 'meble', 'zabawki'],
-        bags: 3,
-        collections: 1,
-        type: 'Fundacja'
-    },
-
-    {
-        name: 'Fundacja "Dbam"',
-        mission: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
-        needs: ['ubrania', 'jedzenie', 'sprzęt AGD', 'meble', 'zabawki'],
-        bags: 3,
-        collections: 2,
-        type: 'Fundacja'
-    },
-
-    {
-        name: 'Fundacja "Dbam o zdrowie 2"',
-        mission: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
-        needs: ['ubrania', 'jedzenie', 'sprzęt AGD', 'meble', 'zabawki'],
-        bags: 3,
-        collections: 2,
-        type: 'Organizacja pozarządowa'
-    },
-
-    {
-        name: 'Fundacja "Zdrowie"',
-        mission: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
-        needs: ['ubrania', 'jedzenie', 'sprzęt AGD', 'meble', 'zabawki'],
-        bags: 3,
-        collections: 1,
-        type: 'Organizacja pozarządowa'
-    },
-
-    {
-        name: 'Fundacja "Dbam"',
-        mission: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
-        needs: ['ubrania', 'jedzenie', 'sprzęt AGD', 'meble', 'zabawki'],
-        bags: 3,
-        collections: 2,
-        type: 'Organizacja pozarządowa'
-    }
+    // {
+    //     name: 'Fundacja "Dbam o zdrowie"',
+    //     mission: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+    //     needs: ['ubrania', 'jedzenie', 'sprzęt AGD', 'meble', 'zabawki'],
+    //     bags: 3,
+    //     collections: 6,
+    //     type: 'Fundacja'
+    // },
+    //
+    // {
+    //     name: 'Fundacja "Dla dzieci"',
+    //     mission: 'Pomoc dzieciom z ubogich rodzin.',
+    //     needs: ['ubrania', 'meble', 'zabawki'],
+    //     bags: 2,
+    //     collections: 1,
+    //     type: 'Fundacja'
+    // },
+    //
+    // {
+    //     name: 'Fundacja "Bez domu"',
+    //     mission: 'Pomoc dla osób nie posiadających miejsca zamieszkania.',
+    //     needs: ['ubrania', 'jedzenie', 'ciepłe koce'],
+    //     bags: 3,
+    //     collections: 2,
+    //     type: 'Fundacja'
+    // },
+    //
+    // {
+    //     name: 'Fundacja "Dbam2"',
+    //     mission: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+    //     needs: ['ubrania', 'jedzenie', 'sprzęt AGD', 'meble', 'zabawki'],
+    //     bags: 3,
+    //     collections: 4,
+    //     type: 'Fundacja'
+    // },
+    //
+    // {
+    //     name: 'Fundacja "Dbam o zdrowie 2"',
+    //     mission: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+    //     needs: ['ubrania', 'jedzenie', 'sprzęt AGD', 'meble', 'zabawki'],
+    //     bags: 3,
+    //     collections: 2,
+    //     type: 'Fundacja'
+    // },
+    //
+    // {
+    //     name: 'Fundacja "Zdrowie"',
+    //     mission: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+    //     needs: ['ubrania', 'jedzenie', 'sprzęt AGD', 'meble', 'zabawki'],
+    //     bags: 3,
+    //     collections: 1,
+    //     type: 'Fundacja'
+    // },
+    //
+    // {
+    //     name: 'Fundacja "Dbam"',
+    //     mission: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+    //     needs: ['ubrania', 'jedzenie', 'sprzęt AGD', 'meble', 'zabawki'],
+    //     bags: 3,
+    //     collections: 2,
+    //     type: 'Fundacja'
+    // },
+    //
+    // {
+    //     name: 'Fundacja "Dbam o zdrowie 2"',
+    //     mission: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+    //     needs: ['ubrania', 'jedzenie', 'sprzęt AGD', 'meble', 'zabawki'],
+    //     bags: 3,
+    //     collections: 2,
+    //     type: 'Organizacja pozarządowa'
+    // },
+    //
+    // {
+    //     name: 'Fundacja "Zdrowie"',
+    //     mission: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+    //     needs: ['ubrania', 'jedzenie', 'sprzęt AGD', 'meble', 'zabawki'],
+    //     bags: 3,
+    //     collections: 1,
+    //     type: 'Organizacja pozarządowa'
+    // },
+    //
+    // {
+    //     name: 'Fundacja "Dbam"',
+    //     mission: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+    //     needs: ['ubrania', 'jedzenie', 'sprzęt AGD', 'meble', 'zabawki'],
+    //     bags: 3,
+    //     collections: 2,
+    //     type: 'Organizacja pozarządowa'
+    // }
 ]
 
 const organisationsReducer = (state = initialState, action) => {
