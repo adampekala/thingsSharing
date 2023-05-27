336cfdafabd84e47c5c0407721f7e4f01b6fea77
adampekala <adaspekala@wp.pl>
2023-05-27 16:03:31 +0200

    styling of form component hero

--- things-sharing/src/E_FormComponent/formComponent.js
+++ things-sharing/src/E_FormComponent/formComponent.js
@@ -10,22 +10,40 @@ import GiveThingsForm from "./giveThingsForm";
 const FormComponent = () => {
     return (
         <>
-            <LoginPanel />
-            <Navigation/>
-            <header id='start' className='header'>
-                <h1 className='headerTitle'>Zacznij pomagać! <br/> Oddaj niechciane rzeczy w zaufane ręce</h1>
-                <img src={Decoration} alt='dekoracja'/>
-                <div className='headerButtons'>
-                    <button type='button'>ODDAJ <br/> RZECZY</button>
-                    <button type='button'>ZORGANIZUJ <br/> ZBIÓRKĘ</button>
+            <main className='formComponent_container'>
+                <div className='formComponent_rightArea'>
+                    <LoginPanel />
+                    <Navigation/>
+                    <header className='formComponent_header'>
+                        <h1 className='formComponent_header-main'>Oddaj rzeczy, których już nie chcesz<br/>POTRZEBUJĄCYM</h1>
+                        <img src={Decoration} alt='dekoracja'/>
+                        <h2 className='formComponent_header-second'>Wystarczą 4 proste kroki:</h2>
+                        <div className='formComponent_header-squares'>
+                            <div className='formComponent_header-square'>
+                                <p>1</p>
+                                <p>Wybierz rzeczy</p>
+                            </div>
+                            <div className='formComponent_header-square'>
+                                <p>2</p>
+                                <p>Spakuj je w worki</p>
+                            </div>
+                            <div className='formComponent_header-square'>
+                                <p>3</p>
+                                <p>Wybierz fundację</p>
+                            </div>
+                            <div className='formComponent_header-square'>
+                                <p>4</p>
+                                <p>Zamów kuriera</p>
+                            </div>
+                        </div>
+                    </header>
                 </div>
-            </header>
+            </main>
             <div className="giveThingsMessage">
                 <h1>Ważne!</h1>
                 <p>Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je przekazać.</p>
             </div>
             <GiveThingsForm/>
-
             <Form />
             <Footer/>
         </>
