70bce8b502b65f13f2dbd02209a25ac11df640d6
adampekala <adaspekala@wp.pl>
2023-05-30 18:57:54 +0200

    add navigation from main page to form

--- things-sharing/src/A_HomePage/a_hero/heroRightArea.js
+++ things-sharing/src/A_HomePage/a_hero/heroRightArea.js
@@ -2,12 +2,13 @@ import React, {useState} from "react";
 import Decoration from '../../Z_Images/Decoration.png'
 import LoginPanel from "../../X_CommonComponents/loginPanel";
 import Navigation from "../../X_CommonComponents/navigation";
+import { useNavigate } from "react-router-dom"
 // import {onAuthStateChanged} from "firebase/auth";
 // import auth from "../../firebase/firebaseConfig";
 
 
 const HeroRightArea = () => {
-
+    const navigate = useNavigate();
     return (
         <div>
             <LoginPanel />
@@ -16,7 +17,8 @@ const HeroRightArea = () => {
                 <h1 className='headerTitle'>Zacznij pomagać! <br/> Oddaj niechciane rzeczy w zaufane ręce</h1>
                 <img src={Decoration} alt='dekoracja'/>
                 <div className='headerButtons'>
-                    <button type='button'>ODDAJ <br/> RZECZY</button>
+                    //TODO to repeat
+                    <button type='button' onClick={() => navigate("/form")}>ODDAJ <br/> RZECZY</button>
                     <button type='button'>ZORGANIZUJ <br/> ZBIÓRKĘ</button>
                 </div>
             </header>
