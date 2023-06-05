8dfde06f6eeffdaf8c37698d21a32de054531770
adampekala <adaspekala@wp.pl>
2023-06-05 20:13:52 +0200

    state development of message form

--- things-sharing/src/A_HomePage/g_form/form.js
+++ things-sharing/src/A_HomePage/g_form/form.js
@@ -1,35 +1,42 @@
 import './form.scss';
 import decoration from '../../Z_Images/Decoration.png'
 import Footer from "../h_footer/footer";
+import {useState} from "react";
 
 
 const Form = () => {
+    const [contactForm, setContactForm] = useState({name: "111", email: "222", message: "333"});
+    const handleOnChangeForm = (event) => {
+        const {name, value} = event.target;
+        setContactForm(prevState => ({...prevState, [name]: value}))
+    }
+
     return (
-        <div id='contact' className='formContainer'>
-            <div className='form'>
+        <div id='contact' className='contactFormContainer'>
+            <div className='contactForm'>
                 <h1>Skontaktuj się z nami</h1>
                 <img className='formDecorationImg' src={decoration} alt='dekoracja'/>
                 <form className='formInputs'>
                     <div className='formInputs_top'>
                         <div>
-                            <label id='name'>Wpisz swoje imię</label>
+                            <label id='contactName'>Wpisz swoje imię</label>
                             <br/>
-                            <input htmlFor='name' value={"dane"}/>
+                            <input type='text' htmlFor='contactName' name='name' value={contactForm.name} onChange={handleOnChangeForm}/>
                         </div>
 
                         <div>
-                            <label id='name'>Wpisz swój email</label>
+                            <label id='contactEmail'>Wpisz swój email</label>
                             <br/>
-                            <input htmlFor='name' value={"dane"}/>
+                            <input type='email' htmlFor='contactEmail' name='email' value={contactForm.email} onChange={handleOnChangeForm}/>
                         </div>
                     </div>
                     <div>
-                        <label id='message'>Wpisz swoją wiadomość</label>
+                        <label id='contactMessage'>Wpisz swoją wiadomość</label>
                         <br/>
-                        <textarea htmlFor='message'/>
+                        <textarea htmlFor='contactEmail' name='message' value={contactForm.message} onChange={handleOnChangeForm}/>
                     </div>
 
-                    <button type='submit' className='formBtnSubmit'>Wyślij</button>
+                    <button type='submit' className='contactFormBtnSubmit'>Wyślij</button>
                 </form>
             </div>
             <Footer/>
